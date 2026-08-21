d497e45d1f7ac0ac3fe0c2728dba29b7257bcfd3
EC2 Default User <ec2-user@ip-10-14-137-50.eu-west-1.compute.internal>
2026-08-21 16:25:04 +0000

    Add Docker skeleton app and ignore build artifacts

---
+++ src/main/java/com/neueda/leap/Main.java
@@ -0,0 +1,7 @@
+package com.neueda.leap;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world from TheLeaping in the firm's Sprint 1 project skeleton");
+    }
+}
