6d57d5b21dc73e4ef6ae13d60c04cdb665f77baa
EC2 Default User <ec2-user@ip-10-14-134-156.eu-west-1.compute.internal>
2026-08-26 18:21:40 +0000

    lab 08 setup and jenkinsfile pipeline setup

--- src/main/java/com/neueda/leap/Main.java
+++ src/main/java/com/neueda/leap/Main.java
@@ -1,8 +1,9 @@
 package com.neueda.leap;
 
 public class Main {
-    public static void main(String[] args) {
-        // TODO: replace <team-name> with your team's actual name
-        System.out.println("Hello world from TheLeaping's Sprint 1 project skeleton");
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Hello world from the team's Sprint 1 project skeleton");
+        System.out.println("Container is up. Sleeping so you can inspect it.");
+        Thread.sleep(600_000);
     }
 }
