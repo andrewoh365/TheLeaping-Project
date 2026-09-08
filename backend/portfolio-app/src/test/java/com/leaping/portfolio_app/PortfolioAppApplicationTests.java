467a41bdf723be4ea86b7d2db3d39ce71010701c
Miguel Trejo <mt353469@gmail.com>
2026-09-08 22:00:16 +0000

    feature: Added Spring Skeleton

---
+++ backend/portfolio-app/src/test/java/com/leaping/portfolio_app/PortfolioAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leaping.portfolio_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
